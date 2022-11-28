51495661c06574c3a17dcb2cc51a21058ae492dc
petridish-dg <mail@peterdg.com>
2022-11-28 03:39:59 -0600

    FixedMenu

--- common/UI/Menu.js
+++ common/UI/Menu.js
@@ -1,19 +1,32 @@
-import { motion as m } from "framer-motion";
+import { motion as m, useCycle } from "framer-motion";
 import Image from "next/image";
 import React from "react";
 
 import s from "./Menu.module.scss";
-import button from "/public/ButtonMenu.svg";
 
 const Menu = () => {
   const cross = {
     focus: {
-      rotate: 90,
+      rotate: 135,
     },
   };
 
   const innerCross = {
-    hover: {
+    initial: {
+      height: 20,
+      width: 20,
+    },
+    idle: {
+      height: 10,
+      width: 10,
+      transition: {
+        repeat: Infinity,
+        repeatType: "reverse",
+        duration: 1.5,
+        ease: "easeInOut",
+      },
+    },
+    onhover: {
       height: 20,
       width: 20,
     },
@@ -22,22 +35,25 @@ const Menu = () => {
       width: 15,
     },
   };
+
+  const [cycle, setCycle] = useCycle(true, false);
+
   return (
     <m.button
       className={s.menuButton}
       variants={cross}
-      whileHover="hover"
+      initial="initial"
+      animate={cycle ? "idle" : "focus"}
+      whileHover="onhover"
       whileFocus="focus"
+      onHoverStart={setCycle}
+      onHoverEnd={setCycle}
     >
       <m.div className={s.crossContainer}>
         <div className={s.menuCross}></div>
         <div className={s.menuCross}></div>
       </m.div>
-      <m.div
-        className={s.menuButtonInner}
-        variants={innerCross}
-        transition={{ ease: "easeOut" }}
-      ></m.div>
+      <m.div className={s.menuButtonInner} variants={innerCross}></m.div>
     </m.button>
   );
 };
